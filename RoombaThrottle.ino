25413b6b06c9547cbd7fdc63af3d7aaafddebcfe
idkaaa <chris.hiles@gmail.com>
2016-08-10 00:18:45 -0400

    cleaned up

--- RoombaThrottle.ino
+++ RoombaThrottle.ino
@@ -10,6 +10,18 @@ int ThrottleInputCurrent = 0;
 //sensor low = 185
 //sensor high = 858
 
+bool IsThrottleActiveLeft = false;
+bool IsThrottleActiveRight = false;
+
+int ThrottleSensorValueLeft = 0;
+int ThrottleSensorValueRight = 0;
+
+int LeftThrottlePwmPin = 5;
+int RightThrottlePwmPin = 10;
+
+int LeftThrottleAnalogSensorPin = A0; 
+int RightThrottleAnalogSensorPin = A5;
+
 int ThrottleSensorValue = 0;
 int ThrottleSensorMinValue = 185;
 int ThrottleSensorMaxValue = 858;
@@ -31,33 +43,86 @@ void setup() {
 void loop() 
 {  
    // put your main code here, to run repeatedly:
-    //Serial.println("In while loop");
-    int SensorValue = 0;
     do
     {
-      SensorValue = analogRead(A0);
+      f_SetActivityThrottle();
+      f_ResetThrottlePwmOutputs();
       delay(20);
-    }while(SensorValue < ThrottleSensorMinValue); //no new signals
+    }while(
+      (IsThrottleActiveLeft == false) 
+    && (IsThrottleActiveRight == false)); //no new signals
     //Serial.print("Sensor value passed threshold.");
-    int ThrottlePwmOutput = f_GetRemappedThrottlePwmOutput(SensorValue);
-    analogWrite(5, ThrottlePwmOutput);
+    f_OutputRemappedThrottleValues();
+}
+
+///send output to motor controllers
+void f_OutputRemappedThrottleValues()
+{
+  if(IsThrottleActiveLeft == true)
+  {
+    int OutputLeft = f_GetRemappedThrottlePwmOutput(ThrottleSensorValueLeft);
+    analogWrite(LeftThrottlePwmPin, OutputLeft);
+  }
+  if(IsThrottleActiveRight == true)
+  {
+    int OutputRight = f_GetRemappedThrottlePwmOutput(ThrottleSensorValueRight);
+    analogWrite(RightThrottlePwmPin, OutputRight);
+  }
+}
+
+///Sets whether throttles are active or not.
+void f_SetActivityThrottle()
+{
+  ThrottleSensorValueLeft = analogRead(LeftThrottleAnalogSensorPin);
+  if(ThrottleSensorValueLeft > ThrottleSensorMinValue)
+  {
+    IsThrottleActiveLeft = true;
+  }
+  else
+  {
+    IsThrottleActiveLeft = false;
+  }
+
+  ThrottleSensorValueRight = analogRead(RightThrottleAnalogSensorPin);
+  if(ThrottleSensorValueRight > ThrottleSensorMinValue)
+  {
+    IsThrottleActiveRight = true;
+  }
+  else
+  {
+    IsThrottleActiveRight = false;
+  }
+}
+
+
+//resets the pwm to 0
+void f_ResetThrottlePwmOutputs()
+{
+  if(IsThrottleActiveLeft == false)
+  {
+    analogWrite(LeftThrottlePwmPin, 0);
+  }
+  if(IsThrottleActiveRight == false)
+  {
+    analogWrite(RightThrottlePwmPin, 0);
+  }
 }
 
 int f_GetRemappedThrottlePwmOutput(int SensorValue)
 {
   int ReMappedSensorValue = SensorValue;
-  int SlowSpeedRangeMax = 500;
-  if(SensorValue < SlowSpeedRangeMax)
-  {
-    ReMappedSensorValue = 
-    map(
-      SensorValue, 
-      ThrottleSensorMinValue, 
-      SlowSpeedRangeMax,
-      ThrottleSensorMinValue,
-      0.5*SlowSpeedRangeMax
-      );
-  }
+//  int SlowSpeedRangeMax = 500;
+//  if(SensorValue < SlowSpeedRangeMax)
+//  {
+//    ReMappedSensorValue = 
+//    map(
+//      SensorValue, 
+//      ThrottleSensorMinValue, 
+//      SlowSpeedRangeMax,
+//      ThrottleSensorMinValue,
+//      0.5*SlowSpeedRangeMax
+//      );
+//  }
     return map(
       ReMappedSensorValue, 
       ThrottleSensorMinValue, 
@@ -65,50 +130,4 @@ int f_GetRemappedThrottlePwmOutput(int SensorValue)
       DutyCycleMin,
       DutyCycleMax
       );
-}
-
-
-///everything below unused?
-
-////gets the voltage coming from the hand throttles
-//float f_GetVoltageFromHandThrottleSensorValue(int SensorValue)
-//{
-//  //different ranges have different mappings to smooth things out.
-//  int MappedSensorValue = 0;
-//  int Range1 = 300;
-//  int Range2 = 600;
-//  int Range3 = 900;
-//  //Range 1:
-//  if(SensorValue < Range1)
-//  {
-//    MappedSensorValue = map(SensorValue, ThrottleSensorMinValue, Range1, )
-//  }
-//  int MappedSensorValue = map(SensorValue, 0)
-//  float Voltage = SensorValue * (5.0 / 1023.0);
-//  //Serial.print("Voltage read from Hand Throttle: ");
-//  //Serial.println(Voltage);
-//  return Voltage;
-//}
-
-//converts throttle voltage input to smoother output for the motor controller
-int f_GetLinearOutputDutyCycleToMotorController(float VoltageFromHandThrottle)
-{
-  //equation to smooth motor response
-  //0.84 volts == 50 duty cycle
-  //4.17 volts == 210 duty cycle
-  //3.33 volts range
-  //3.33 * 100 = 333
-  //Normalized sensor min 0 = 50 duty cycle
-  //Normalized sensor max 333 = 210 duty cycle
-  //each step = 2.08125 (range normalized sensor/range duty cycle)
-  //each output = 2.08125*input
-
-  float NormalizedVoltage = (VoltageFromHandThrottle - ThrottleSensorVoltageMin) * 100;
-  int RangeSensor = (ThrottleSensorVoltageMax - ThrottleSensorVoltageMin)*100; //3.24 to 324
-  int RangeDutyCycle = DutyCycleMax - DutyCycleMin;
-  float ConversionConstant = RangeSensor / RangeDutyCycle;
-  float LinearOutput = NormalizedVoltage / ConversionConstant;
-  //Serial.print("Linear duty cycle output: ");
-  //Serial.println(LinearOutput);
-  return (int)LinearOutput + DutyCycleMin;
 }
